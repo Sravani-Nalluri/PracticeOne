7a225ab833818da22cf1dc49ee90683a96ebaf5a
Sravani Nalluri <sravani.nalluri@zemosolabs.com>
2022-05-25 16:50:34 +0530

    my first commit

---
+++ src/com/metoverload/MethodoOver.java
@@ -0,0 +1,25 @@
+package com.metoverload;
+
+public class MethodoOver {
+	
+	private String formatNumber(int value) {
+        return String.format("%d", value);
+    }
+
+    private String formatNumber(double value) {
+        return String.format("%.3f", value);
+    }
+
+    private String formatNumber(String value) {
+        return String.format("%.2f", Double.parseDouble(value));
+    }
+
+    public static void main(String[] args) {
+    	MethodoOver  hs = new 	MethodoOver ();
+        System.out.println(hs.formatNumber(500));
+        System.out.println(hs.formatNumber(89.9934));
+        System.out.println(hs.formatNumber("550"));
+    }
+	
+
+}
